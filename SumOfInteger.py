19f50c377fd76b9f65441b6554512e9d391cdd79
Chetna Patel <99310118+Chetna100@users.noreply.github.com>
2025-07-23 22:54:37 +0530

    Add files via upload
    It Takes an integer input from the user and Checks whether the number is even or odd using an if-else statement. Displays the result accordingly.

---
+++ SumOfInteger.py
@@ -0,0 +1,4 @@
+sum_of_numbers = 0
+for i in range(1, 51):  # range(1, 51) includes 1 but excludes 51, thus covering 1 to 50
+    sum_of_numbers += i
+print("The sum of Numbers from 1 to 50 is: ", sum_of_numbers)
